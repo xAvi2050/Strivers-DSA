8a34bd3afcfa8a1a571628693bf3a14ee07f299f
xAvi2050 <cheetahmindset7@gmail.com>
2025-06-25 17:30:38 +0530

    Final Commit

--- Hashing.java
+++ Hashing.java
@@ -10,30 +10,68 @@
 //         // Print frequencies of all numbers
 //         for (int i = 0; i < hash.length; i++) {
 //             if (hash[i] > 0) {
-//                 System.out.println("Number " + i + " -- " + hash[i] + " times");
+//                 System.out.println("Number " + i + " --> " + hash[i] + " times");
 //             }
 //         }
 //     }
 // }
 
 
+// public class Hashing {
+//     public static void main(String[] args) {
+//         String str = "abcdAbcfffg";
+//         int[] hash = new int[256];
+
+//         for(int i=0; i<str.length(); i++){
+            
+//             hash[str.charAt(i)]++;
+//         }
+
+//         for(int i=0; i<hash.length; i++){
+//             if(hash[i]>0){
+//                 System.out.println("Character " + (char)(i) + " --> " + hash[i] + " times");
+//             }
+//         }
+//     }
+// }
+
+// --> Hash Map for counting elements in an array of numbers
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hashing {
     public static void main(String[] args) {
-        String str = "abcdAbcfffg";
-        int[] hash = new int[256];
+        
+        int[] arr = {2, 4, 2, 7, 8, 8, 5, 4, 4, 6, 8};
 
-        for(int i=0; i<str.length(); i++){
-            
-            hash[str.charAt(i)]++;
+        Map<Integer, Integer> ele = new HashMap<>();
+
+        for (int num : arr) {
+            ele.put(num, ele.getOrDefault(num, 0) + 1);
         }
 
-        for(int i=0; i<hash.length; i++){
-            if(hash[i]>0){
-                System.out.println("Character " + (char)(i) + " -- " + hash[i] + " times");
+        int maxFreq = Integer.MIN_VALUE;
+        int minFreq = Integer.MAX_VALUE;
+        int mostFrequent = -1;
+        int leastFrequent = -1;
+
+        for (Map.Entry<Integer, Integer> entry : ele.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+
+            if (freq > maxFreq) {
+                maxFreq = freq;
+                mostFrequent = num;
+            }
+
+            if (freq < minFreq) {
+                minFreq = freq;
+                leastFrequent = num;
             }
         }
+
+        System.out.println("Most frequent → " + mostFrequent + " (" + maxFreq + " times)");
+        System.out.println("Least frequent → " + leastFrequent + " (" + minFreq + " time" + (minFreq > 1 ? "s" : "") + ")");
     }
 }
-
-// --> Hash Map
-
--- Test.java
+++ Test.java
@@ -32,24 +32,3 @@
 //         sc.close();
 //     }
 // }
-
-// public class Test {
-//     public static void modifyPrimitive(int number) {
-//         number = number + 10;
-//     }
-
-//     public static void modifyArray(int[] arr) {
-//         arr[0] = arr[0] + 10;
-//     }
-
-//     public static void main(String[] args) {
-//         // int a = 5;
-//         int[] array = {5};
-
-//         modifyPrimitive(5);
-//         modifyArray(array);
-
-//         System.out.println("Value of a after modifyPrimitive: " + a);
-//         System.out.println("Value of array[0] after modifyArray: " + array[0]);
-//     }
-// }
