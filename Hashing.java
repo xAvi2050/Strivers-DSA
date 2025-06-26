84a88579d738f802120416458d21064454dc5cbe
xAvi2050 <cheetahmindset7@gmail.com>
2025-06-26 13:52:29 +0530

    Final Commit

--- Hashing.java
+++ Hashing.java
@@ -1,10 +1,10 @@
 // public class Hashing {
 //     public static void main(String[] args) {
 //         int[] arr = {2, 4, 2, 7, 8, 8, 5, 4, 4, 6, 8};
-//         int[] hash = new int[9];  // max element is 8
+//         int[] hash = new int[9]; 
 
 //         for (int i = 0; i < arr.length; i++) {
-//             hash[arr[i]]++;  // Count frequency of each number
+//             hash[arr[i]]++;  
 //         }
 
 //         // Print frequencies of all numbers
@@ -37,41 +37,45 @@
 
 // --> Hash Map for counting elements in an array of numbers
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-public class Hashing {
-    public static void main(String[] args) {
+// public class Hashing {
+//     public static void main(String[] args) {
         
-        int[] arr = {2, 4, 2, 7, 8, 8, 5, 4, 4, 6, 8};
+//         int[] arr = {2, 4, 2, 7, 6, 7, 8, 8, 5, 5, 4, 4, 6, 8};
 
-        Map<Integer, Integer> ele = new HashMap<>();
+//         Map<Integer, Integer> ele = new HashMap<>();
 
-        for (int num : arr) {
-            ele.put(num, ele.getOrDefault(num, 0) + 1);
-        }
+//         for (int num : arr) {
+//             if(ele.containsKey(num)){
+//                 ele.put(num, ele.get(num)+1);
+//             }else{
+//                 ele.put(num, 1);
+//             }
+//         }
 
-        int maxFreq = Integer.MIN_VALUE;
-        int minFreq = Integer.MAX_VALUE;
-        int mostFrequent = -1;
-        int leastFrequent = -1;
+//         int maxFreq = Integer.MIN_VALUE;
+//         int minFreq = Integer.MAX_VALUE;
+//         int mostFrequentElement = -1;
+//         int leastFrequentElement = -1;
 
-        for (Map.Entry<Integer, Integer> entry : ele.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
+//         for (Map.Entry<Integer, Integer> entry : ele.entrySet()) {
+//             int num = entry.getKey();
+//             int freq = entry.getValue();
 
-            if (freq > maxFreq) {
-                maxFreq = freq;
-                mostFrequent = num;
-            }
+//             if (freq > maxFreq) {
+//                 maxFreq = freq;
+//                 mostFrequentElement = num;
+//             }
 
-            if (freq < minFreq) {
-                minFreq = freq;
-                leastFrequent = num;
-            }
-        }
+//             if (freq < minFreq) {
+//                 minFreq = freq;
+//                 leastFrequentElement = num;
+//             }
+//         }
 
-        System.out.println("Most frequent → " + mostFrequent + " (" + maxFreq + " times)");
-        System.out.println("Least frequent → " + leastFrequent + " (" + minFreq + " time" + (minFreq > 1 ? "s" : "") + ")");
-    }
-}
+//         System.out.println("Most frequent --> " + mostFrequentElement + " (" + maxFreq + " times)");
+//         System.out.println("Least frequent --> " + leastFrequentElement + " (" + minFreq + " time" + (minFreq > 1 ? "s" : "") + ")");
+//     }
+// }
