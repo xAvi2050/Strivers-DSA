043d61ddd8e7c00665b739920fb499515f884c67
xAvi2050 <cheetahmindset7@gmail.com>
2025-08-13 23:36:38 +0530

    final commit

---
+++ Strings.java
@@ -0,0 +1,46 @@
+public class Strings {
+
+    // 1) Reverse a string
+    // public static void main(String[] args) {
+    //     String str = "avijit";
+    //     char[] charArray = str.toCharArray();
+        
+    //     int left = 0, right = charArray.length - 1;
+        
+    //     while (left < right) {
+    //         char temp = charArray[left];
+    //         charArray[left] = charArray[right];
+    //         charArray[right] = temp;
+    //         left++;
+    //         right--;
+    //     }
+        
+    //     // Now print reversed
+    //     for (char c : charArray) {
+    //         System.out.print(c + " ");
+    //     }
+    // }
+
+    // 2) Check if a string is a palindrome 
+    // public static void main(String[] args) {
+    //     String str = "madamx";
+
+    //     if(isPalindrome(str)) System.out.println("The given string is palindrome");
+    //     else System.out.println("The given string is not a palindrome");
+    // }
+
+    // static boolean isPalindrome(String str){
+    //     int left = 0, right = str.length() - 1;
+
+    //     while(left<right){
+    //         if(str.charAt(left) != str.charAt(right)) return false;
+    //         left++; right--;
+    //     }
+    //     return true;
+    // }
+
+    // 3) Are two strings anagrams? 
+    public static void main(String[] args) {
+        
+    }
+}
